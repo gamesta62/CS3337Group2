9372caf759370bdde3736f88461325e2619b5fb8
Kamron Mahmoodzadeh <kzadeh1@gmail.com>
2018-12-03 07:03:41 +0000

    Add simple navbar to main pages
    Navbar contains links to main page, module 2, module 3, and settings,
    and appears on each of those pages.

--- HSS/src/project/Index.java
+++ HSS/src/project/Index.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Index
  */
-@WebServlet("/Index")
+@WebServlet("")
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
